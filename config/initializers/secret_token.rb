d98452f140979be24723dbeb45efb0f84f4e2f75
Bruce Williams <bruwillimail@gmail.com>
2013-03-10 10:35:37 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scanhandler::Application.config.secret_token = '9680adc59db376e4f0bda2f82a93dfec35da58ebfcd3f8f5d0ae498133f130bffbb3987d320464b0bba179a1209e33e2c3bb33ec86fe734ff007d7e390945f71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scanhandler::Application.config.session_store :cookie_store, key: '_scanhandler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scanhandler::Application.config.session_store :active_record_store
