915e94a0b9be0f725efec5df8cb94d270da33f6e
Bruce Williams <bruwillimail@gmail.com>
2013-03-10 19:50:33 -0700

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Scanhandler::Application.config.secret_token = 'ae0f3d9d2d4163b567a381f75aad592577fc7344cfae73fa7fc9f41980656d092e287980008a802c86c9e0cd97eb4160f5586a432a86feab07718ba3e8c72bb4'
+Scanhandler::Application.config.secret_token = 'e765adf2a0620a6f5d6e4a2e0d45a6d7b57c63a210b095c6556e8c3068c40abf0e49d98b587c0a52185e08173b1b3e2ab4a403e5e9b770470c4b46abea17d191'
