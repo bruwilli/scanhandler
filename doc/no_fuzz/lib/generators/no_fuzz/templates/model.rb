1f7228f1824fb000e3edbfdf895777e8c987a679
Bruce Williams <bruwillimail@gmail.com>
2013-03-20 20:37:32 -0700

    Removed unused no_fuzz files

--- doc/no_fuzz/lib/generators/no_fuzz/templates/model.rb
+++
@@ -1,4 +0,0 @@
-class Trigrams::<%= flattened_class_name -%> < ActiveRecord::Base
-  belongs_to :<%= belongs_to_association %>, :class_name => "::<%= class_name %>"
-  
-end
--- doc/no_fuzz/lib/generators/no_fuzz/templates/module.rb
+++
@@ -1,7 +0,0 @@
-module Trigrams
-  
-  def self.table_name_prefix
-    '<%= table_name_prefix -%>'
-  end
-  
-end
