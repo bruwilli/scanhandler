91c1607a2c6690d7c70b176e7687fa7012f6a14d
Bruce Williams <bruwillimail@gmail.com>
2013-05-24 07:28:56 -0700

    Use local storage for development, s3 for production

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,5 +1,3 @@
-require 'open-uri'
-
 class ScansController < ApplicationController
   before_filter :authenticate_user!
   respond_to :html, :json, :xml
@@ -29,12 +27,9 @@ class ScansController < ApplicationController
    redirect_to @scan.person
    end
 
-
    def download
    authorize! :download, Scan, :message => 'Not authorized to download scans.'
    @scan = Scan.find(params[:id])
-   data = open(@scan.image.url(:pdf))
-   send_data data.read, :type => data.content_type, :x_sendfile => true
-   #send_file @scan.image.path(:pdf), type: 'application/pdf', filename: @scan.image_file_name
+   send_file @scan.image.path(:pdf), type: 'application/pdf', filename: @scan.image_file_name
    end
 end
