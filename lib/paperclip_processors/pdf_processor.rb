e07c95e94fb43d5ce1ba7bb6af31cf413ce4599b
Bruce Williams <bruwillimail@gmail.com>
2013-05-29 12:21:58 -0700

    Modified pdf processor

--- lib/paperclip_processors/pdf_processor.rb
+++ lib/paperclip_processors/pdf_processor.rb
@@ -16,18 +16,19 @@ module Paperclip
 
     def make
       begin
-        if @current_format != "pdf"
-          image = Magick::Image.read(@file.path).first
+        path = @current_format == "pdf" ? (@file.path + "[0]") : @file.path
+        #if @current_format != "pdf"
+          image = Magick::Image.read(path).first.resize_to_fit(1650)
           image.format = 'PDF'
           @current_format = "pdf"
           tmp = Tempfile.new([@basename, @current_format].compact.join("."))
-          image.write( tmp.path)
+          image.write( tmp.path) { self.quality = 75 }
           tmp.flush
           @attachment.instance_write(:content_type, "application/pdf")
           @attachment.instance_write(:file_size, tmp.size)
 
           @file = tmp
-        end
+        #end
                  
         @file
       rescue StandardError => e
