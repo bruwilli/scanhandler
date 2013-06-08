92f9fe17a1cfdbf0ef586bb5de9fae4d7eb43268
Bruce Williams <bruwillimail@gmail.com>
2013-06-08 15:47:07 -0700

    Fix invitation accept url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Scanhandler::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'floating-gorge-6706.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'personnel.arsenalfcsandiego.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
