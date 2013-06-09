9b6fe650c1f7d38bc90a4b19207d00a6b767b56d
Bruce Williams <bruwillimail@gmail.com>
2013-06-08 20:21:11 -0700

    Pass thru 1-page pdf files

--- lib/paperclip_processors/pdf_processor.rb
+++ lib/paperclip_processors/pdf_processor.rb
@@ -16,16 +16,23 @@ module Paperclip
 
     def make
       begin
-        path = @current_format == "pdf" ? (@file.path + "[0]") : @file.path
-        image = Magick::Image.read(path){ self.density = 150 } .first.resize_to_fit(1650)
-        image.format = 'PDF'
-        @current_format = "pdf"
-        tmp = Tempfile.new([@basename, @current_format].compact.join("."))
-        image.write( tmp.path) { self.quality = 75 }
-        tmp.flush
-        @attachment.instance_write(:content_type, "application/pdf")
-        @attachment.instance_write(:file_size, tmp.size)
-        @file = tmp
+        #path = @current_format == "pdf" ? (@file.path + "[0]") : @file.path
+        images = Magick::Image.read(path)
+        if (images.length > 0 || @current_format != "pdf") {
+          if (@current_format == "pdf") {
+            image = Magick::Image.read(path + "[0]") { self.density = 150 }
+          } else {
+            image = images.first.resize_to_fit(1650)
+          }
+          image.format = 'PDF'
+          @current_format = "pdf"
+          tmp = Tempfile.new([@basename, @current_format].compact.join("."))
+          image.write( tmp.path) { self.quality = 75 }
+          tmp.flush
+          @attachment.instance_write(:content_type, "application/pdf")
+          @attachment.instance_write(:file_size, tmp.size)
+          @file = tmp
+        }
                  
         @file
       rescue StandardError => e
