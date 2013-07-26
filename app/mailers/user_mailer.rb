e09e6ab357a3a93e2a55a1fd7ae4fceb3d5852aa
Bruce Williams <bruwillimail@gmail.com>
2013-07-26 15:54:32 -0700

    fixed list of users that get mailed scan notifications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@4sranchsoccer.com"
 
   def send_scan_notification_email(person, user, scan)
-    notified_users = User.where("send_notification = ?", true).reject { |notified_user| !notified_user.roles.first.nil? }
+    notified_users = User.where("send_notification = ?", true).reject { |notified_user| notified_user.roles.first.nil? }
     emails = notified_users.map { |notified_user| notified_user.email }
     @user = user
     @person = person
