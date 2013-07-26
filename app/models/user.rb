97444ab8117cc9cf73f1401e4fd6c35064c9c1fd
Bruce Williams <bruwillimail@gmail.com>
2013-07-25 21:04:58 -0700

    Fixed changing of scan notification setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :role_ids, :as => :admin
+  attr_accessible :role_ids, :send_notification, :as => :admin
   attr_accessible :name, :send_notification, :email, :password, :password_confirmation, :remember_me
 
   has_many :people
