f84661ca4cc3bbc5cdbadf1f306c79c5a2b52c43
Bruce Williams <bruwillimail@gmail.com>
2014-01-31 06:41:42 -0800

    Added config to production to allow heroku pushes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Scanhandler::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.assets.initialize_on_precompile = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
